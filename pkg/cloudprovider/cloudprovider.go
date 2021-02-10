847da1a0c27b32d9632a08b0d262c34e96e517c5
Alexander Constantinescu <aconstan@redhat.com>
2021-02-10 09:41:46 +0100

    Use fsnotify to reconcile secret data
    Signed-off-by: Alexander Constantinescu <aconstan@redhat.com>

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -3,8 +3,14 @@ package cloudprovider
 import (
 	"fmt"
 	"net"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"gopkg.in/fsnotify/fsnotify.v1"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
 )
 
 const (
@@ -48,4 +54,56 @@ func NewCloudProviderClient(cloudProvider string) (CloudProviderIntf, error) {
 }
 
 func (c *CloudProvider) WatchForSecretChanges() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		klog.Fatal(err)
+	}
+	defer watcher.Close()
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					klog.Infof("Cloud provider secret data has been re-written, re-initializing credentials")
+					if err := c.intf.InitCredentials(); err != nil {
+						klog.Errorf("Error re-initializing credentials, will send SIGTERM and shutdown, err: %v", err)
+						// Don't do os.Exit here, instead trigger a SIGTERM which will call our
+						// signal handlers and initiate a controlled shutdown of all controllers
+						syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+					}
+				}
+			case _, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+			}
+		}
+	}()
+	// Secrets are symlinks, where one synlink is created for each `.data`
+	// attribute in the secret. Since `.data` depends on the cloud provider,
+	// watch as many as 10. If any one of these gets re-written, we kill ourselves.
+	p := cloudProviderSecretLocation
+	maxdepth := 10
+	for depth := 0; depth < maxdepth; depth++ {
+		if err := watcher.Add(p); err != nil {
+			klog.Fatal(err)
+		}
+		stat, err := os.Lstat(p)
+		if err != nil {
+			klog.Fatal(err)
+		}
+		if stat.Mode()&os.ModeSymlink > 0 {
+			p, err = filepath.EvalSymlinks(p)
+			if err != nil {
+				klog.Fatal(err)
+			}
+		} else {
+			break
+		}
+	}
+	<-done
 }
